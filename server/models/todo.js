636b353e98f60d0388bb87fac9d18ffaf48bb41d
ASiDesigner <asadzadeh.ali@gmail.com>
2018-12-27 17:23:26 +0330

    Add POST /todos route and refactor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+let Todo = mongoose.model('Todo',{
+    text:{
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    compeleted:{
+        type: Boolean,
+        default: false
+    },
+    comeletedAt:{
+        type: Number,
+        default: null
+    }
+});
+
+module.exports = {Todo};
+
