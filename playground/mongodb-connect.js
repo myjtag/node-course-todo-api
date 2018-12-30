58a43713b1ddf0955a64d983af0bc75cd0a4b328
ASiDesigner <asadzadeh.ali@gmail.com>
2018-12-30 12:23:14 +0330

    Updated mongoose to work with mlab.com

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -4,7 +4,8 @@ const {MongoClient, ObjectID} = require('mongodb');
 // let obj = new ObjectID();
 // console.log(obj);
 
-MongoClient.connect('mongodb://localhost:27017',(err,client)=>{
+//MongoClient.connect('mongodb://localhost:27017',(err,client)=>{
+MongoClient.connect('mongodb://admin:welcome110@ds145484.mlab.com:45484/todos', { useNewUrlParser: true }, (err, client) => {
     if(err){
         return console.log('Unable to connect to mogoDB server');
     }
@@ -27,19 +28,19 @@ MongoClient.connect('mongodb://localhost:27017',(err,client)=>{
     // });
 
     //Insert new doc into Users (name,age,location)
-    const db = client.db('TodoApp');
+    const db = client.db('todos');
     
-    // db.collection('Users').insertOne({
-    //     name:'ASiDesigner',
-    //     age:36,
-    //     location:'shiraz'
-    // },(err,res)=>{
-    //     if(err){
-    //         return console.log('Unable to insert Users',err);
-    //     }
-    //     console.log(JSON.stringify(res.ops,undefined,2));
-    //     console.log(res.ops[0]._id.getTimestamp());
-    // });
+    db.collection('Users').insertOne({
+        name:'ASiDesigner',
+        age:36,
+        location:'shiraz'
+    },(err,res)=>{
+        if(err){
+            return console.log('Unable to insert Users',err);
+        }
+        console.log(JSON.stringify(res.ops,undefined,2));
+        console.log(res.ops[0]._id.getTimestamp());
+    });
 
     client.close();
 });
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise= global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://admin:welcome110@ds145484.mlab.com:45484/todos' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 
 module.exports = {  mongoose };
