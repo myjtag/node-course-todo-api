b9317dd8237db08ae29c4de6f0bc6e4cfa9c5869
ASiDesigner <asadzadeh.ali@gmail.com>
2018-12-30 13:18:19 +0330

    Add Delete /todos/:id route

--- server/server.js
+++ server/server.js
@@ -46,9 +46,29 @@ app.get('/todos/:id',(req,res)=>{
 
     }).catch((e)=>{
         res.status(400).send();
-    })
+    });
 });
 
+
+app.delete('/todos/:id',(req,res)=>{
+    
+    let id = req.params.id;
+    console.log(id,'id is this');
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    
+    Todo.findByIdAndRemove(id).then((doc)=>{
+        if(!doc){
+            return res.status(404).send();
+        }
+
+        res.send(doc);
+
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+});
 
 app.listen(port,()=>{
     console.log(`Started on port ${port}`);
