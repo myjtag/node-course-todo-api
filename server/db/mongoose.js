73609cab918fc21d4cdba5ea247c73675f18bc73
ASiDesigner <asadzadeh.ali@gmail.com>
2019-01-01 12:36:09 +0330

    Setup seprate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise= global.Promise;
-mongoose.connect('mongodb://admin:welcome110@ds145484.mlab.com:45484/todos' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true });
+
 
 
 module.exports = {  mongoose };
+
