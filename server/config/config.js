175f6a6c47e7d102503097fb3ff7f35f979d1da0
ASiDesigner <asadzadeh.ali@gmail.com>
2019-01-01 12:56:05 +0330

    Update heroku error

--- server/config/config.js
+++ server/config/config.js
@@ -13,3 +13,4 @@ if(env === 'development'){
 
 }
 
+
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise= global.Promise;
-mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://admin:welcome110@ds145484.mlab.com:45484/todos', { useNewUrlParser: true });
 
 
 
