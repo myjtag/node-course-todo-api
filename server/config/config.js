2226013e77ed01989d858bcc40838539573d1e17
ASiDesigner <asadzadeh.ali@gmail.com>
2019-01-02 10:13:30 +0330

    Corrected Heroku and git SSH

--- server/config/config.js
+++ server/config/config.js
@@ -3,9 +3,9 @@ let env = process.env.NODE_ENV || 'development';
 
 if(env === 'development'){
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://admin:welcome110@ds145484.mlab.com:45484/todos';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 }else if (env === 'test'){
-    // process.env.PORT = 3000;
+    process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 }else{ // production
     // process.env.PORT = 3000;
