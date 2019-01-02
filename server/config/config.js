7128c6652b2bbcc45b2c1d656bf33c991dff3770
ASiDesigner <asadzadeh.ali@gmail.com>
2019-01-02 16:13:08 +0330

    Add GET /users/me and authenticate middle ware

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ let env = process.env.NODE_ENV || 'development';
 
 if(env === 'development'){
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp2';
 }else if (env === 'test'){
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
