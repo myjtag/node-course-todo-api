30dec66f8cc402412b632c65852c25a5bea64aa8
ASiDesigner <asadzadeh.ali@gmail.com>
2019-01-12 11:58:08 +0330

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,25 @@
 let env = process.env.NODE_ENV || 'development';
 
+if(env === 'development' || env === 'test'){
+    let config = require('./config.json');
+    let envConfig = config[env];
 
-if(env === 'development'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp2';
-}else if (env === 'test'){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}else{ // production
-    // process.env.PORT = 3000;
-    // process.env.MONGODB_URI = 'mongodb://admin:welcome110@ds145484.mlab.com:45484/todos';
-    
+   Object.keys(envConfig).forEach((key)=>{
+    process.env[key] = envConfig[key];
+   });
+   
 }
+
+// if(env === 'development'){
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp2';
+// }else if (env === 'test'){
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }else{ // production
+//     // process.env.PORT = 3000;
+//     // process.env.MONGODB_URI = 'mongodb://admin:welcome110@ds145484.mlab.com:45484/todos';
+    
+// }
 
 
